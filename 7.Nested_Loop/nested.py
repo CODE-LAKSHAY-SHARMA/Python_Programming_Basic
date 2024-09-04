5ec80bcaeca040cf1d89dbd19740ccd540c126cd
lakshay <lakshaysh19@gmail.com>
2024-09-04 15:12:35 +0530

    Nested Loop concept

---
+++ 7.Nested_Loop/nested.py
@@ -0,0 +1,6 @@
+# basic concept of Nested for loop
+
+for i in range(1, 4):
+    print(i)
+    for j in range(100, 103):
+        print(j)
---
+++ 7.Nested_Loop/pattern1.py
@@ -0,0 +1,18 @@
+"""
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+"""
+
+# # basic approach using print function
+# for i in range(5):
+#     print("X X X X X")
+
+# using nested for loop to solve the pattern
+
+for i in range(5):
+    for j in range(5):
+        print("X", end=" ")
+    print()
